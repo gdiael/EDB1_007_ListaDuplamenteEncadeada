b71741e972b8dde675308630e01c3bf551ada349
Gdiael Barros <gdiael.barros@gmail.com>
2018-10-09 20:40:47 -0300

    Lista Implementada com sucesso!

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -78,43 +78,121 @@ string LinkedList::get(int i)
 
 int LinkedList::search(string s)
 {
-	std::cout << "\tERRO - Método search ainda não foi implementado.\n";
+	// implementação de busca
+	int counter = 1;
+    
+    for(Node<string>* n = this->head->getNext(); n != this->tail; n = n->getNext())
+    {
+        if( s == n->getValue() )
+        {
+            return counter;
+        }
+        counter++;
+    }
+
+	std::cerr << "A string [" << s << "] não foi encontrada na lista encadeada!." << std::endl;
     return -1;
 }
 
 bool LinkedList::insertBegin(string s)
 {
-   	std::cout << "\tERRO - Método insertBegin ainda não foi implementado.\n";
-    return false;
+	Node<string>* novo = new Node<string>(s);
+	novo->Insert(this->head, this->head->getNext());
+   	this->quantity++;
+    return true;
 }
 
 bool LinkedList::insertEnd(string s)
 {
-	std::cout << "\tERRO - Método insertEnd ainda não foi implementado.\n";
-    return false;
+	Node<string>* novo = new Node<string>(s);
+	novo->Insert(this->tail->getPrevious(), this->tail);
+	this->quantity++;
+    return true;
 }
 
 bool LinkedList::insert(int i, string s)
 {
-    std::cout << "\tERRO - Método insert ainda não foi implementado.\n";
+	if( i < 1 || i > this->quantity+1)
+    {
+        std::cerr << "Valor do índice de inserção deve estar entre [1, " << this->quantity+1 << "]." << std::endl;
+        return false;
+    }
+		
+    int counter = 1;
+    
+    for(Node<string>* n = this->head; n != this->tail; n = n->getNext())
+    {
+        if( i == counter )
+        {
+            Node<string>* novo = new Node<string>(s);
+			novo->Insert(n, n->getNext());
+			this->quantity++;
+            return true;
+        }
+        counter++;
+    }
+    
     return false;
 }
 
 string LinkedList::removeEnd(void)
 {
-    std::cout << "\tERRO - Método removeEnd ainda não foi implementado.\n";
-    return "";
+	if( this->isEmpty())
+	{
+		std::cerr << "A lista está vazia, não há elementos a remover!" << std::endl;
+        return "";
+	};
+    Node<string>* toRemove = this->tail->getPrevious();
+	string str = toRemove->getValue();
+	toRemove->Remove();
+	delete toRemove;
+	this->quantity--;
+    return str;
 }
 
 string LinkedList::removeBegin(void)
 {
-    std::cout << "\tERRO - Método removeBegin ainda não foi implementado.\n";
-    return "";
+	if( this->isEmpty())
+	{
+		std::cerr << "A lista está vazia, não há elementos a remover!" << std::endl;
+        return "";
+	};
+    Node<string>* toRemove = this->head->getNext();
+	string str = toRemove->getValue();
+	toRemove->Remove();
+	delete toRemove;
+	this->quantity--;
+    return str;
 }
 
 string LinkedList::remove(int indice)
 {
-    std::cout << "\tERRO - Método remove ainda não foi implementado.\n";
+    if( indice < 1 || indice > this->quantity)
+    {
+        std::cerr << "Valor do índice para remoção deve estar entre [1, " << this->quantity << "]." << std::endl;
+        return "";
+    };
+	if( this->isEmpty() )
+	{
+		std::cerr << "A lista está vazia, não há elementos a remover!" << std::endl;
+        return "";
+	};
+		
+    int counter = 1;
+    
+    for(Node<string>* n = this->head->getNext(); n != this->tail; n = n->getNext())
+    {
+        if( indice == counter )
+        {
+            string str = n->getValue();
+			n->Remove();
+			delete n;
+			this->quantity--;
+            return str;
+        }
+        counter++;
+    };
+    
     return "";
 }
 
